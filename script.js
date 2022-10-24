dfb8535b72fed8c20b87c3c66cfef5564be5578d
Michael Walker <mikewalk517@gmail.com>
2022-10-24 10:06:43 -0400

    yer a wizard

---
+++ script.js
@@ -0,0 +1,24 @@
+for (i = 1; i < 7; i++){
+    console.log(i);
+}
+
+for (i=5; i <= 25; i += 4) {
+    console.log(i);
+}
+
+const wizards = [
+    `Harry Potter`,
+    `Hermione Granger`,
+    `Ron Weasley`,
+];
+
+for (wiz of wizards){
+    console.log(wizards);
+}
+
+let harryPotterMovies = 0;
+
+while (harryPotterMovies <= 8){
+    console.log(harryPotterMovies);
+    harryPotterMovies++;
+}
